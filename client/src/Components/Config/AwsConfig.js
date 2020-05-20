cfbb30d00ec5cce7332f32f80e8b5d62c332108d
Isral <IsralHadero@gmail.com>
2020-05-20 18:27:37 -0400

    gitignore

---
+++ client/src/Components/Config/AwsConfig.js
@@ -0,0 +1,6 @@
+export const Awsconfig = {
+  bucketName: 'house-images-list',
+  region: 'us-east-2',
+  accessKeyId: 'AKIAJGHNKFVU5CSRVWXA',
+  secretAccessKey: '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'
+}
---
+++ server/src/config/awsConfig.js
@@ -0,0 +1,8 @@
+const awsconfig = {
+  bucketName: 'house-images-list',
+  region: 'us-east-2',
+  accessKeyId: 'AKIAJGHNKFVU5CSRVWXA',
+  secretAccessKey: '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'
+}
+
+module.exports = awsconfig
