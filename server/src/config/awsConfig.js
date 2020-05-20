e4cdab8651a61b8b86d02decafd103e0fa337a82
Isral <IsralHadero@gmail.com>
2020-05-20 18:08:05 -0400

    changge

--- server/src/config/awsConfig.js
+++
@@ -1,8 +0,0 @@
-const awsconfig = {
-  bucketName: 'house-images-list',
-  region: 'us-east-2',
-  accessKeyId: 'AKIAJ3TQN3LE3V7ADVNA',
-  secretAccessKey: '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'
-}
-'ssssss'
-module.exports = awsconfig
