fc2159d80c6cfcc3adf60ac2360900e73eca0803
Isral <IsralHadero@gmail.com>
2020-06-13 04:11:56 -0400

    remove config files

--- client/src/Components/Config/AwsConfig.js
+++
@@ -1,6 +0,0 @@
-export const Awsconfig = {
-  bucketName: 'house-images-list',
-  region: 'us-east-2',
-  accessKeyId: 'AKIAJGHNKFVU5CSRVWXA',
-  secretAccessKey: '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'
-}
--- server/src/config/awsConfig.js
+++
@@ -1,7 +0,0 @@
-const awsconfig = {
-  bucketName: 'property-listing-project',
-  region: 'us-east-2',
-  accessKeyId: 'AKIAJ7MOHGYJX5QJHXRA',
-  secretAccessKey: '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'
-}
-module.exports = awsconfig
